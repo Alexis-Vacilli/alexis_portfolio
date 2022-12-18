d181149daa4b33d38dd6b072e50e52c4c65b65e6
Alexis-Vacilli <alexisvacilli1@gmail.com>
2022-12-18 12:13:55 +0200

    netlify bug fix

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React, { Suspense } from 'react'
 import ReactDOM from 'react-dom'
 import i18next from 'i18next'
-import { initReactI18next, useTranslation } from 'react-i18next'
+import { initReactI18next } from 'react-i18next'
 import HttpApi from 'i18next-http-backend'
 import LanguageDetector from 'i18next-browser-languagedetector'
 import App from './App'
